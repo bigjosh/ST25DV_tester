f7cf1c2af387b0a5434b776f1c690236a77d049a
josh <github@junk.josh.com>
2021-08-01 02:20:58 -0400

    OK< can write a mailbox message that can be read over NFC!
    Phew that was fricken hard.
    
    To read on ST NFC demo app, do....
    
    Custom Command
    Addressed Mode
    CMD_CODE = 0xac
    Manufacturer checked (0x02)
    Data: 00 00 (this is for the pointer to where to start reading in the mailbox buffer and len. 00,00 mean start at beginning and read whole thing)

--- ST25DV_tester.ino
+++ ST25DV_tester.ino
@@ -1,5 +1,11 @@
 
 
+
+#define SCL_PIN 5
+#define SCL_PORT PORTC
+#define SDA_PIN 4
+#define SDA_PORT PORTC
+
 #include "SoftI2CMaster.h"
 
 
@@ -16,38 +22,32 @@ void setup() {
   Serial.println("\r\nNFC JOSH Start...");  
 
   // put your setup code here, to run once:
-  Wire.begin();   // Join as master
 
-  Wire.setClock( 10000 );
-
-  //st25dv.ST25DV_SelectI2cSpeed(0);
-  
+  i2c_init();
+    
 }
 
 
 
 
-byte i2cWrite( const byte *pData, const uint8_t devAddr, const uint16_t TarAddr,  uint16_t len) {
+byte i2cWrite( const byte *pData, const uint8_t addr, const uint16_t TarAddr,  uint16_t len) {
 
-  byte addr = devAddr >> 1;         // Loose bottom r/w bit
+  i2c_start(addr|I2C_WRITE );    // Get the slave's attention, tell it we're sending a command byte
+  i2c_write(TarAddr >> 8);           //  The command byte, sets pointer to register with address of 0x32
+  i2c_write(TarAddr & 0xFF);         //  The command byte, sets pointer to register with address of 0x32
 
-  Wire.beginTransmission(addr);    // Get the slave's attention, tell it we're sending a command byte
-  Wire.write(TarAddr >> 8);           //  The command byte, sets pointer to register with address of 0x32
-  Wire.write(TarAddr & 0xFF);         //  The command byte, sets pointer to register with address of 0x32
-
-  Wire.write(pData,len);
-  
-  byte ret = Wire.endTransmission(true);
-
-  if (ret) {
-
-    Serial.print( ret ); 
-    Serial.println( "=W endTransmission fail." );                  
-    
-    //return 1;
+  Serial.print("WRITE:");
+  while (len--) {
+    Serial.print( (int) *pData , 16 );
+    Serial.print( ' ' );
+    i2c_write(*pData++);
   }
 
-  return ret;
+  i2c_stop();
+
+  Serial.println(".");
+
+  return 0;
 
 }
 
@@ -73,42 +73,26 @@ void i2cPresentPassord( const byte *pData) {
 }
 
 
-byte i2cRead( byte *pData, const uint8_t devAddr, const uint16_t TarAddr,  uint16_t len) {
+byte i2cRead( byte *pData, const uint8_t addr, const uint16_t TarAddr,  uint16_t len) {
 
-  byte addr = devAddr >> 1;
 
-  Wire.beginTransmission(addr);    // Get the slave's attention, tell it we're sending a command byte
-  Wire.write(TarAddr >> 8);           //  The command byte, sets pointer to register with address of 0x32
-  Wire.write(TarAddr & 0xFF);         //  The command byte, sets pointer to register with address of 0x32
+  i2c_start(addr|I2C_WRITE);    // Get the slave's attention, tell it we're sending a command byte
+  i2c_write(TarAddr >> 8);           //  The command byte, sets pointer to register with address of 0x32
+  i2c_write(TarAddr & 0xFF);         //  The command byte, sets pointer to register with address of 0x32
+
   
-  byte ret = Wire.endTransmission(false);
-
-  if (ret) {
-
-    Serial.print( ret ); 
-    Serial.println( "=W endTransmission fail." );                  
+  i2c_rep_start(addr|I2C_READ);
     
-    return 1;
-  }
-  
-  Wire.requestFrom( addr ,  len  ); // Tell slave we need to read 1byte from the current register
-  
-  while ( len && Wire.available()) {
-    byte c = Wire.read();      // read that byte into 'slaveByte2' variable
+  while ( len ) {
+
+    len--;
+
+    byte c = i2c_read(len==0 );      // the param here is called "last" and is true if this is the lasy byte to read
     *pData = c;
     pData++;
-    len--;
   }
 
-  ret = Wire.endTransmission();
-
-  if (ret) {
-
-    Serial.print( ret ); 
-    Serial.println( "=R endTransmission fail." );                  
-    
-    //return 1;
-  }
+  i2c_stop();
 
 
   return 0;
@@ -151,7 +135,7 @@ void loop() {
 
 
 
-  const byte password_in_array[] = {0x00,0x00,0x00,0x00,0x00,0x00};    // Default password
+  const byte password_in_array[] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};    // Default password
   
   i2cPresentPassord( password_in_array );
   Serial.println("Presented password.");                                                    
@@ -162,84 +146,47 @@ void loop() {
   Serial.println( b[0] , 16 );           
 
                                             
-  i2cWrite(  one_in_array , ST25DV_ADDR_DATA_I2C , 0x000D , 0x0001 );
-  i2cRead( b , ST25DV_ADDR_DATA_I2C , 0x000D , 0x0001 );
+  i2cWrite(  one_in_array , ST25DV_ADDR_SYST_I2C , 0x000D , 0x0001 );
+  delay(5);
+  i2cRead( b , ST25DV_ADDR_SYST_I2C , 0x000D , 0x0001 );
   Serial.print("MB_MODE=1:");                                                      
   Serial.println( b[0] , 16 );                                                    
 
 
   i2cRead( b , ST25DV_ADDR_DATA_I2C , 0x2006 , 0x0001 );
-  Serial.print("MB_CTRL=?:");                                                        
+  Serial.print("MB_CTRL=?:");
   Serial.println( b[0] , 16 );   
 
   i2cWrite(  one_in_array , ST25DV_ADDR_DATA_I2C , 0x2006 , 0x0001 );
+  delay(5);
   i2cRead( b , ST25DV_ADDR_DATA_I2C , 0x2006 , 0x0001 );
   Serial.print("MB_CTRL=1:");                                                      
   Serial.println( b[0] , 16 );                                                            
 
 
-  i2cRead( b , ST25DV_ADDR_SYST_I2C , 0x000D , 0x0001 );
+  i2cRead( b , ST25DV_ADDR_SYST_I2C , 0x000E , 0x0001 );
   Serial.print("MB_WDG:");                                                      
   Serial.println( b[0] , 16 );                                                            
-  i2cWrite(  zero_in_array , ST25DV_ADDR_SYST_I2C , 0x000D , 0x0001 );
-  i2cRead( b , ST25DV_ADDR_SYST_I2C , 0x000D , 0x0001 );
+  i2cWrite(  zero_in_array , ST25DV_ADDR_SYST_I2C , 0x000E , 0x0001 );
+  delay(5);
+  i2cRead( b , ST25DV_ADDR_SYST_I2C , 0x000E , 0x0001 );
   Serial.print("MB_WDG=0:");                                                      
   Serial.println( b[0] , 16 );                                                            
 
 
-
   const byte magic_message_in_array[] = { 0x62 , 0x63 };      
   i2cWrite(  magic_message_in_array , ST25DV_ADDR_DATA_I2C , 0x2008 , 0x0002 );
-  Serial.println( "Wroite message into mailbox." );   
+  delay(5);
+  Serial.println( "Wrote message into mailbox." );   
   
   i2cRead( b , ST25DV_ADDR_DATA_I2C , 0x2006 , 0x0001 );
   Serial.print("MB_CTRL=?:");                                                        
   Serial.println( b[0] , 16 );                                                    
   
-
-
   i2cRead( b , ST25DV_ADDR_DATA_I2C , 0x2007 , 0x0001 );
   Serial.print("MB_LEN=?:");                                                        
   Serial.println( b[0] , 16 );                                                    
-
-  i2cRead( b , ST25DV_ADDR_DATA_I2C , 0x2006 , 0x0001 );
-  Serial.print("MB_CTRL=?:");                                                        
-  Serial.println( b[0] , 16 );                                                    
-
-
-  i2cWrite(  magic_message_in_array , ST25DV_ADDR_DATA_I2C , 0x2008 , 0x0002 );
-  Serial.println( "Wroite message into mailbox." );                                                    
-  
-  i2cRead( b , ST25DV_ADDR_DATA_I2C , 0x2007 , 0x0001 );
-  Serial.print("MB_LEN=2:");                                                        
-  Serial.println( b[0] , 16 );                                                    
-                                           
-
-
-  i2cRead( b , ST25DV_ADDR_DATA_I2C , 0x2000 , 0x001 ); 
-  Serial.print("GPIO_CONTRL:");
-  Serial.println( b[0] , 16 );                                                    
-
-  Serial.println("Writing 0x08 to GPIOI_CTRL.");  
-  i2cWrite(  eight_in_array , ST25DV_ADDR_DATA_I2C , 0x2000 , 0x0001 );
-  
-  
-  i2cRead( b , ST25DV_ADDR_DATA_I2C , 0x2000 , 0x001 ); 
-  Serial.print("GPIO_CONTRL:");
-  Serial.println( b[0] , 16 );                                                    
-
-  const byte eightyeight_in_array[] = {0x88};
-
-
-  Serial.println("Writing 0x88 to GPIOI_CTRL.");  
-  i2cWrite(  eightyeight_in_array , ST25DV_ADDR_DATA_I2C , 0x2000 , 0x0001 );
-  
-  
-  i2cRead( b , ST25DV_ADDR_DATA_I2C , 0x2000 , 0x001 ); 
-  Serial.print("GPIO_CONTRL:");
-  Serial.println( b[0] , 16 );                                                    
-
-
+                                          
 
   i2cRead( b , ST25DV_ADDR_SYST_I2C , 0x17 , 0x001 ); 
   Serial.print("IC_REF:");                                                      
@@ -247,80 +194,18 @@ void loop() {
   
 
   i2cRead( b , ST25DV_ADDR_SYST_I2C , 0x18 , 0x008 ); 
-
-
-  Serial.print("UUID:");                                                    
-
-  
+  Serial.print("UUID:");                                                      
   for( int i=0; i<0x08; i++ ) {
           Serial.print( (unsigned) b[i] , 16 );        
           Serial.print( ' ' );                                                    
-    }
-  
-
+    }  
   Serial.println();        
 
 
-  i2cRead( b , ST25DV_ADDR_SYST_I2C , 0x0d , 0x001 ); 
-
-
-  Serial.print("MB_MODE:");                                                    
-  
-  Serial.println( b[0] , 16 );            
-
-
-
   i2cRead( b , ST25DV_ADDR_DATA_I2C , 0x2004 , 0x0001 );
-
-  Serial.print("I2C_SSO:");                                                    
+  Serial.print("I2C_SSO=1:");                                                      
+  Serial.println( b[0] , 16 );           
   
-  Serial.println( b[0] , 16 );                                                    
-
-
-
-  
-  i2cPresentPassord( password_in_array );
-  
-
-  Serial.println("Presented password.");                                                    
-    
-
-  i2cRead( b , ST25DV_ADDR_DATA_I2C , 0x2004 , 0x0001 );
-
-  Serial.print("I2C_SSO:");                                                    
-  
-  Serial.println( b[0] , 16 );                                                    
-
-                                          
-                                            
-  i2cWrite(  one_in_array , ST25DV_ADDR_DATA_I2C , 0x000D , 0x0001 );
-
-  i2cRead( b , ST25DV_ADDR_DATA_I2C , 0x000D , 0x0001 );
-
-  Serial.print("MB_MODE=1:");                                                    
-  
-  Serial.println( b[0] , 16 );                                                    
-
-
-  i2cWrite( zero_in_array, ST25DV_ADDR_DATA_I2C , 0x2006 , 0x0001 );
-
-  i2cRead( b , ST25DV_ADDR_DATA_I2C , 0x2006 , 0x0001 );
-
-
-  Serial.print("MB=0:");                                                    
-  
-  Serial.println( b[0] , 16 );   
-                                                   
-  
-  i2cWrite( one_in_array , ST25DV_ADDR_DATA_I2C , 0x2006 , 0x0001 );
-
-  i2cRead( b , ST25DV_ADDR_DATA_I2C , 0x2006 , 0x0001 );
-
-  Serial.print("MB=1:");                                                    
-
-  Serial.println( b[0] , 16 );
-
-                                               
 
   
   while (1);
